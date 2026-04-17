2b00733a7a4f688271ac407f452d0574b8cfe0b2
joeali31 <ya9415565@gmail.com>
2026-04-17 12:04:15 -0700

    Question13

--- LINQsolution02/Program.cs
+++ LINQsolution02/Program.cs
@@ -139,6 +139,19 @@
 
             #endregion
 
+            #region Question13
+            //int[] setA = { 1, 3, 5, 7, 9, 11, 13 };
+            //int[] setB = { 3, 6, 9, 12, 15, 13 };
+
+            //var res = setA.Except(setB);
+
+            //foreach (var item in res)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
 
         }
     }
